c4e8e89d7b1895fa6aa1bbe0915ca3d1f8c873d9
Mordechai <demarpacman@gmail.com>
2024-01-12 02:58:45 +0700

    Menu Screen 1.2
    adding detail for buttons and credits

--- Its Just A Matter Of Time/Assets/Scripts/UI/PlayerManager.cs
+++ Its Just A Matter Of Time/Assets/Scripts/UI/PlayerManager.cs
@@ -27,7 +27,6 @@ public class PlayerManager : MonoBehaviour
             HealthManager.health = 0;
             rb.bodyType = RigidbodyType2D.Static;
             animator.SetTrigger("isDead");
-            Time.timeScale = 0;
             Debug.Log("Game Over");
         }
 
