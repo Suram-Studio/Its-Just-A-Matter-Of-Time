0a83461d4f10c894344664c5069a870b47992eab
Mordechai <demarpacman@gmail.com>
2024-01-12 14:58:19 +0700

    Updated Changes

---
+++ Its Just A Matter Of Time/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Control : MonoBehaviour
+{
+    public void BackToMenu()
+    {
+       SceneManager.LoadScene("Menu");
+    }
+}
